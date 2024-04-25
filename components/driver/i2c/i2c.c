4722986de1874f551acd1f96b8fad12edc343a99
Marshal Barrett <marshal@barrelogix.net>
2024-04-25 09:38:25 -0500

    when i2c timeout is zero using default timeout

--- components/driver/i2c/i2c.c
+++ components/driver/i2c/i2c.c
@@ -1545,7 +1545,7 @@ esp_err_t i2c_master_cmd_begin(i2c_port_t i2c_num, i2c_cmd_handle_t cmd_handle, 
         TickType_t wait_time = xTaskGetTickCount();
 
         // 4-22-24 MDB: Modified to accept whatever wait time
-        // is provided
+        // is provided unless 0, in which case it will default
         /*
         if (wait_time - ticks_start > ticks_to_wait) { // out of time
             wait_time = I2C_CMD_ALIVE_INTERVAL_TICK;
@@ -1556,7 +1556,7 @@ esp_err_t i2c_master_cmd_begin(i2c_port_t i2c_num, i2c_cmd_handle_t cmd_handle, 
             }
         }
         */
-        wait_time = ticks_to_wait;
+        wait_time = ticks_to_wait == 0 ? I2C_CMD_ALIVE_INTERVAL_TICK: ticks_to_wait;
 
         // In master mode, since we don't have an interrupt to detective bus error or FSM state, what we do here is to make
         // sure the interrupt mechanism for master mode is still working.
